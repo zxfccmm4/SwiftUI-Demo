88ddd35704e2194f9a9698517064068b47dc50d8
Steve Zhou <zxfccmm@126.com>
2022-05-27 15:59:49 +0800

    Add files via upload

---
+++ Stevedemo/PageView/PageView.swift
@@ -0,0 +1,29 @@
+//
+//  PageView.swift
+//  Stevedemo
+//
+//  Created by Steve Zhou (周晓峰) on 2022/5/27.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    @State private var currentPage = 0
+
+    var body: some View {
+        ZStack(alignment: .bottomTrailing) {
+            PageViewController(pages: pages, currentPage: $currentPage)
+            PageControl(numberOfPages: pages.count, currentPage: $currentPage)
+                .frame(width: CGFloat(pages.count * 18))
+                .padding(.trailing)
+        }
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
